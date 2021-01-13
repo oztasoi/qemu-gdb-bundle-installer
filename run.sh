1b653a12e94849ffa18e03867bb6fe6bad1d06bf
oztasozgurcan <oztas.ozgurcan@gmail.com>
2021-01-13 13:28:14 +0300

    Another hotfix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker pull 0xyg3n/cmpe443qemuimage:v1.0
-docker run -p 1234:1234 -p 6942:6942 -v $(pwd)/$1:/test.axf -it 0xyg3n/cmpe443qemuimage:v1.0 /usr/local/bin/qemu-system-arm -machine LPC4088 -kernel /test.axf -nographic -s -S
+docker run -p 1234:1234 -p 6942:6942 -v $1:/test.axf -it 0xyg3n/cmpe443qemuimage:v1.0 /usr/local/bin/qemu-system-arm -machine LPC4088 -kernel /test.axf -nographic -s -S
