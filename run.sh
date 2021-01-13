15d78bbdeaa2de6ba9b4f350d8da1969bdf38f53
oztasozgurcan <oztas.ozgurcan@gmail.com>
2021-01-13 14:09:12 +0300

    Added debug

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 docker pull 0xyg3n/cmpe443qemuimage:v1.0
 
 FNAME=$(echo "$(cd $(dirname $1); pwd -P)/$(basename $1)")
+echo "I am here."
 docker run -p 1234:1234 -p 6942:6942 -v $FNAME:/$1 -it 0xyg3n/cmpe443qemuimage:v1.0 /usr/local/bin/qemu-system-arm -machine LPC4088 -kernel /$1 -nographic -s -S
