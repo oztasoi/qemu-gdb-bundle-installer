b1c548fd39c49aadd7efb100f78ad6c4f68889d6
oztasozgurcan <oztas.ozgurcan@gmail.com>
2021-01-13 14:06:08 +0300

    File directory name unmatch fixed.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker pull 0xyg3n/cmpe443qemuimage:v1.0
 
-FNAME=$(echo "$(cd $(dirname prog.elf); pwd -P)/$(basename prog.elf)")
-docker run -p 1234:1234 -p 6942:6942 -v  $FNAME:/test.axf -it 0xyg3n/cmpe443qemuimage:v1.0 /usr/local/bin/qemu-system-arm -machine LPC4088 -kernel /test.axf -nographic -s -S
+FNAME=$(echo "$(cd $(dirname $1); pwd -P)/$(basename $1)")
+docker run -p 1234:1234 -p 6942:6942 -v $FNAME:/test.axf -it 0xyg3n/cmpe443qemuimage:v1.0 /usr/local/bin/qemu-system-arm -machine LPC4088 -kernel /test.axf -nographic -s -S
