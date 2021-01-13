03b492e06c59e3cfe183225117531ef72739fdee
Mahmut Karaca <mahmutkaraca95@gmail.com>
2021-01-13 13:42:55 +0300

    add automatic filename grab functionality

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 docker pull 0xyg3n/cmpe443qemuimage:v1.0
-docker run -p 1234:1234 -p 6942:6942 -v $1:/test.axf -it 0xyg3n/cmpe443qemuimage:v1.0 /usr/local/bin/qemu-system-arm -machine LPC4088 -kernel /test.axf -nographic -s -S
+
+FNAME=$(echo "$(cd $(dirname prog.elf); pwd -P)/$(basename prog.elf)")
+docker run -p 1234:1234 -p 6942:6942 -v  $FNAME:/test.axf -it 0xyg3n/cmpe443qemuimage:v1.0 /usr/local/bin/qemu-system-arm -machine LPC4088 -kernel /test.axf -nographic -s -S
